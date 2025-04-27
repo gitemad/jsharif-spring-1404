507422b790d86a9473a70fa1ef244120271bb566
Emad <sudani.emad@gmail.com>
2025-04-27 20:12:50 +0330

    feat: rename imported module example

--- 06-modular-programming.py
+++ 06-modular-programming.py
@@ -2,11 +2,16 @@ import math
 
 print(math.pi)
 
-from math import (
-    sqrt,
-    sin,
-)
-# from math import *
+# from math import (
+#     sqrt,
+#     sin,
+# )
+# # from math import *
 
-print(sqrt(4))
-print(sin(0))
+# print(sqrt(4))
+# print(sin(0))
+
+import math as m
+
+print(m.sin(m.pi))
+
